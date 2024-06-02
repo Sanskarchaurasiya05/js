23972d50acae8643b72f2c896036550f9fc9d0fe
Sanskarchaurasiya05 <sanskarvns19@gmail.com>
2024-06-02 06:50:22 +0000

    datatype done

---
+++ basic/02_datatype.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+ 
+//  alert(3+3) with alert we not use arithmetic operation bcz this is the node not document like html
+// OR we are using node js, not browser 
+// console.log(3+3); console.log("sanskar")
+// code must be readable not like above(code redability should be high)
+console.log(3+3)
+console.log("sanskar") //this is good
+ let name="sanskar" // string data type
+ let age=18 //integer
+ let isloggedIn=false //false
+// dataType
+// number => 2^53 (range)
+// bigint
+// string => ""
+// boolean=false/true
+// null =>stand alone value (is a datatype) representation of empty(means vo empty nhi hai vo khali hai)
+// undefined =>value not assigned
+// symbol => for uniqueness
+
+
+// object
+
+
+console.log(typeof "sanskar")
+console.log(typeof age)
+
+console.log(typeof null)
