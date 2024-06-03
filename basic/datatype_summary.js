4ad69350501b5da07c714c607401f06508cc41dd
Sanskarchaurasiya05 <sanskarvns19@gmail.com>
2024-06-03 17:16:04 +0000

    datatype summ done

--- basic/datatype_summary.js
+++ basic/datatype_summary.js
@@ -34,3 +34,30 @@
 
 // how to know datatype of any number 
 console.log(typeof heros);
+
+
+
+
+// *****************************************************************************************************************
+// stack memory(primitive type)=>jab bhi stack memory use hoti hai jo bhi variable humne declare kiya uski copy milti hai.
+// Heap memory (non primitive type)=> original value ka reference milta hai.
+
+let myyoutubename="sanskar" //=> saari primitive value stack me jaati hai. 
+
+let anothername=myyoutubename
+anothername="krishna"
+console.log(myyoutubename)
+console.log(anothername);
+
+let user1={
+    email:"usergoo@.com",         // All non primitive types go into the heap and heap gives reference of original value.
+    upi:"user@ybl"
+}
+
+let user2=user1
+
+// how to access in object by using dot(.)
+
+user2.email="sanskarmico@.com"
+console.log(user1.email);
+console.log(user2.email);
