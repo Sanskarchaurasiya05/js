6ab46b0c18e1f76991b4a02c85ff73d19e8e0f94
Sanskarchaurasiya05 <sanskarvns19@gmail.com>
2024-06-12 15:23:44 +0000

    object3 done

---
+++ 02basic/03_object.js
@@ -0,0 +1,32 @@
+// destruicturing of object 
+
+const course={
+  coursename: "js in hindi",
+  price: "999",
+  courseinstructure: "sanskar"
+} 
+
+// how to use the value and for cleaning we have a syntax
+ 
+// const {courseinstructor} = course
+const {courseinstructor: instructor} = course
+console.log(instructor);
+
+
+// API
+// JSON
+
+
+// {
+//    "name": "sanskar".
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+// API in array format 
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
